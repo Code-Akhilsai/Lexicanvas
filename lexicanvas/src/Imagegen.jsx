eb05bc11636d367797120b94ed36541e0f26a51e
Akhilsai <codeakhilsai@gmail.com>
2025-06-08 11:15:07 +0530

    UI

--- lexicanvas/src/Imagegen.jsx
+++ lexicanvas/src/Imagegen.jsx
@@ -3,33 +3,30 @@ import { IoMdSearch } from "react-icons/io";
 const Imagegen = () => {
   return (
     <>
-      <div className="relative w-[300px] h-[500px] rounded-3xl my-15 ml-auto mr-auto sm:w-[89vw] ">
+      <div className="relative w-[95%] h-[500px] rounded-3xl my-15 ml-auto mr-auto sm:w-[89vw] ">
         <div className="absolute inset-0 bg-[url('./assets/img.png')] bg-cover bg-no-repeat bg-center rounded-3xl opacity-75"></div>
         <div className="relative z-10 flex flex-col items-center justify-center h-full">
-          <p className="text-3xl font-bold font-sans text-white text-center mb-3 sm:mb-4 text-4xl">
+          <p className="text-3xl font-bold font-sans text-white text-center mb-3 sm:mb-4 lg:text-5xl ">
             Unlesh your imagination with AI-Powered Art
           </p>
-          <p className="text-[13px] font-sans text-white text-center sm:text-[14px] ">
+          <p className="text-[13px] font-sans text-white text-center sm:text-[14px] lg:text-[16px]">
             Transform your thoughts into stunning visuals with our advanced
             text-to-image AI generator.
           </p>
 
           <div className="relative w-[85%] my-7.5">
-            <span className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400">
+            <span className="absolute left-2 top-1/2 -translate-y-1/2 text-gray-400 md:left-[2%] lg:left-[20%]">
               <IoMdSearch size={20} />
             </span>
             <input
               type="text"
-              placeholder="Describe your vision"
-              className="bg-[#162238] w-full h-13 rounded-2xl text-white px-10 pr-24 md:w-[60%]"
+              placeholder="Describe"
+              className="bg-[#162238] w-[105%] h-13 rounded-2xl text-white px-10 pr-20 ml-auto mr-auto  lg:w-[65%] lg:ml-[19%] "
             />
-            <button className="absolute right-2 top-1/2 -translate-y-1/2 bg-[#359EFF] h-10 w-[90px] rounded-[11px] text-white font-sans hidden sm:block ">
+            <button className="absolute right-[-3%] top-1/2 -translate-y-1/2 bg-[#359EFF] h-10 w-[75px] rounded-[11px]  text-white font-sans sm:w-[78px] sm:right-[-20px]  md:w-[85px] md:right-[-4%] lg:right-[16.5%]">
               Generate
             </button>
           </div>
-          <button className="bg-[#359EFF] h-10 w-[90px] rounded-[11px] text-white font-sans sm:hidden">
-            Generate
-          </button>
         </div>
       </div>
     </>

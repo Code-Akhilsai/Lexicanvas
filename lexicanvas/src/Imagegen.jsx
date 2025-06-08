39a9b799ae69b803a9703e356d24bf6472fe7f0e
Akhilsai <codeakhilsai@gmail.com>
2025-06-08 11:36:35 +0530

    Logo font size increased

--- lexicanvas/src/Imagegen.jsx
+++ lexicanvas/src/Imagegen.jsx
@@ -3,7 +3,7 @@ import { IoMdSearch } from "react-icons/io";
 const Imagegen = () => {
   return (
     <>
-      <div className="relative w-[95%] h-[500px] rounded-3xl my-15 ml-auto mr-auto sm:w-[89vw] ">
+      <div className="relative w-[95%] h-[500px] rounded-3xl my-18 ml-auto mr-auto sm:w-[89vw] ">
         <div className="absolute inset-0 bg-[url('./assets/img.png')] bg-cover bg-no-repeat bg-center rounded-3xl opacity-75"></div>
         <div className="relative z-10 flex flex-col items-center justify-center h-full">
           <p className="text-3xl font-bold font-sans text-white text-center mb-3 sm:mb-4 lg:text-5xl ">
@@ -15,13 +15,10 @@ const Imagegen = () => {
           </p>
 
           <div className="relative w-[85%] my-7.5">
-            <span className="absolute left-2 top-1/2 -translate-y-1/2 text-gray-400 md:left-[2%] lg:left-[20%]">
-              <IoMdSearch size={20} />
-            </span>
             <input
               type="text"
-              placeholder="Describe"
-              className="bg-[#162238] w-[105%] h-13 rounded-2xl text-white px-10 pr-20 ml-auto mr-auto  lg:w-[65%] lg:ml-[19%] "
+              placeholder="Create an image..."
+              className="bg-[#162238] w-[105%] h-13 rounded-2xl text-white px-4  ml-auto mr-auto  lg:w-[65%] lg:ml-[19%] "
             />
             <button className="absolute right-[-3%] top-1/2 -translate-y-1/2 bg-[#359EFF] h-10 w-[75px] rounded-[11px]  text-white font-sans sm:w-[78px] sm:right-[-20px]  md:w-[85px] md:right-[-4%] lg:right-[16.5%]">
               Generate
--- lexicanvas/src/Menubar.jsx
+++ lexicanvas/src/Menubar.jsx
@@ -2,7 +2,7 @@ const Menubar = () => {
   return (
     <>
       <div className="fixed top-0 left-0 w-full z-50 bg-[#162238] border-b border-[#0e1729]">
-        <p className="font-medium ml-2 mt-1.5 mb-1.5 text-1xl text-white ">
+        <p className="font-medium ml-2 mt-1.5 mb-1.5 text-1xl text-white lg:text-2xl">
           Lexicanvas
         </p>
       </div>
